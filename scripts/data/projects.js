fc367d0eba50c679a53b2eb7ca921edca958cbbc
Pascal <pas111cal3@gmail.com>
2025-10-03 01:50:24 +0100

    updated links to whatsapp

--- scripts/data/projects.js
+++ scripts/data/projects.js
@@ -9,7 +9,7 @@ export const myProjects = [
       ],
     name:'Around CampusNG',
     description: 'Is a student focused housing platform, that allow student\'s post hostel space\'s easily , which they can make money from or find a space around thier respective college with ease!',
-    longDescription:'Around Campus Nigeria (ACNG) is a web app that makes it easier for students to find, filter, and share hostel accommodations. I built the platform using modular JavaScript, applying event delegation to manage dynamic UI events efficiently and OOP principles to keep the code structured and reusable. I also leveraged array methods (map, filter, forEach) for rendering and filtering listings, while connecting to a backend API with Fetch to load and submit data in real time. The design emphasizes responsive layouts, clean UI components, and usability, making the app functional across devices.',
+    longDescription:'is a web app that makes it easier for students to find, filter, and share hostel accommodations. I built the platform using modular JavaScript, applying event delegation to manage dynamic UI events efficiently and OOP principles to keep the code structured and reusable. I also leveraged array methods (map, filter, forEach) for rendering and filtering listings, while connecting to a backend API with Fetch to load and submit data in real time. The design emphasizes responsive layouts, clean UI components, and usability, making the app functional across devices.',
     tools : ['HTML', 'CSS', 'JAVASCRIPT', 'MONGO DB', 'NODE JS', 'EXPRESS'],
     moreInfo: '', 
     url: 'https://aroundcampus.ng',
@@ -28,8 +28,8 @@ export const myProjects = [
     ],
     name:'Quiz Campus',
     description: 'A tool/app that helps\'s student practice past questions on the platform rather than the old paper and biro, which requires alot to start',
-    longDescription:'QuizCampus is a lightweight web app built to help Nigerian students practice GST (General Studies) courses through interactive quizzes. I implemented it using vanilla JavaScript, HTML, and CSS, focusing on dynamic question rendering and user interaction. The quiz logic was built with loops, conditionals, and array methods (map, forEach, filter) to manage questions, track answers, and calculate scores in real time. I also applied DOM manipulation to update the UI dynamically, showing feedback and results without page reloads. The design is responsive and simple, making it easy for students to access the quizzes on any device.',
-    tools : ['HTML', 'CSS', 'JAVASCRIPT', 'MONGO DB', 'NODE JS', 'EXPRESS'],
+    longDescription:'is a lightweight web app built to help Nigerian students(UNIBEN) practice GST (General Studies) courses through interactive quizzes. I implemented it using vanilla JavaScript, HTML, and CSS, focusing on dynamic question rendering and user interaction. The quiz logic was built with loops, conditionals, and array methods (map, forEach, filter) to manage questions, track answers, and calculate scores in real time. I also applied DOM manipulation to update the UI dynamically, showing feedback and results without page reloads. The design is responsive and simple, making it easy for students to access the quizzes on any device.',
+    tools : ['HTML', 'CSS', 'JAVASCRIPT'],
     moreInfo: '', 
     url: 'https://quiz-campus.vercel.app/',
        extraInfo:{
@@ -48,7 +48,7 @@ export const myProjects = [
     name:'wePay',
     description: 'is a payment solution prototype I’m building with a team of four. As the Frontend Developer, I focus on the UI and user interactions while collaborating with a PM, Backend, and QA to deliver a smooth payment experience.',
 
-    longDescription:"WePay is a payment solution prototype I’m building in a team of four: a Project Manager, Backend Developer, QA Engineer, and myself as the Frontend Developer. I focus on crafting the UI and user interactions, while the backend handles secure payment flows and QA ensures stability.This project is sharpening my frontend skills and giving me hands-on experience in team collaboration, agile workflow, and cross-functional development as we work toward a usable prototype.",
+    longDescription:" A payment solution prototype currently being built, in a team of four: a Project Manager, Backend Developer, QA Engineer, and myself as the Frontend Developer. I focus on crafting the UI and user interactions, while the backend handles secure payment flows and QA ensures stability.This project is sharpening my frontend skills and giving me hands-on experience in team collaboration, agile workflow, and cross-functional development as we work toward a usable prototype.",
     tools : ['HTML', 'CSS', 'JAVASCRIPT'],
       moreInfo: '', 
     url: 'https://we-pay.netlify.app/',
