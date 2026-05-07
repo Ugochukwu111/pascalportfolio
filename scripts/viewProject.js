4f073a76966fc8f37e96fb5e762c211c074c60d3
Pascal <pas111cal3@gmail.com>
2026-05-07 13:40:11 +0100

    feat: implement dynamic document title

--- scripts/viewProject.js
+++ scripts/viewProject.js
@@ -77,6 +77,7 @@ let githubIcon = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
 
 
 function updateProjectDetails(project) {
+  document.title = `${project.name} - Pascal's Portfolio `;
   nameEl.textContent = project.name || 'project name';
   descriptionEl.textContent = project.description;
   longDescriptionEl.textContent = project.longDescription;
